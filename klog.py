413a6d600643b316d566773592f482c1ff017cea
RangS <rangga19sj@gmail.com>
2025-11-28 17:22:17 +0700

    Add handling for additional key inputs in klog.py
    Adding function button, alt, altGr, Page Up, Page Down

--- klog.py
+++ klog.py
@@ -48,6 +48,46 @@ class KL:
 						file.write(" [U] ")
 					elif i == "Key.down":
 						file.write(" [D] ")
+					elif i == "Key.alt":
+						file.write(" [alt] ")
+					elif i == "Key.alt_l":
+						file.write(" [al] ")
+					elif i == "Key.alt_r":
+						file.write(" [ar] ")
+					elif i == "Key.alt_gr":
+						file.write(" [agr] ")
+					elif i == "Key.home":
+						file.write(" [H] ")
+					elif i == "Key.end":
+						file.write(" [E] ")
+					elif i == "Key.page_up":
+						file.write(" [PU] ")
+					elif i == "Key.page_down":
+						file.write(" [PD] ")
+					elif i == "Key.f1":
+						file.write(" [F1] ")
+					elif i == "Key.f2":
+						file.write(" [F2] ")
+					elif i == "Key.f3":
+						file.write(" [F3] ")
+					elif i == "Key.f4":
+						file.write(" [F4] ")
+					elif i == "Key.f5":
+						file.write(" [F5] ")
+					elif i == "Key.f6":
+						file.write(" [F6] ")
+					elif i == "Key.f7":
+						file.write(" [F7] ")
+					elif i == "Key.f8":
+						file.write(" [F8] ")
+					elif i == "Key.f9":
+						file.write(" [F9] ")
+					elif i == "Key.f10":
+						file.write(" [F10] ")
+					elif i == "Key.f11":
+						file.write(" [F11] ")
+					elif i == "Key.f12":
+						file.write(" [F12] ")
 					else:
 						file.write(i)
 
